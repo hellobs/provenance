8e2a8c8fcd21e4e97b4d9c6669f93bfba5227835
Ruige <879919878@qq.com>
2026-08-30 15:31:17 +0800

    feat: 素材收集

---
+++ provenance/frontend/static/assets/village/tilemap/trading_hall_pro5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TradingHallPro5" tilewidth="32" tileheight="32" tilecount="3640" columns="70">
+ <image source="trading_hall_pro5.png" width="2240" height="1664"/>
+</tileset>
---
+++ provenance/frontend/static/assets/village/tilemap/trading_hall_ref.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="TradingHallRef" tilewidth="32" tileheight="32" tilecount="3640" columns="70">
+ <image source="trading_hall_ref.png" width="2240" height="1664"/>
+</tileset>
