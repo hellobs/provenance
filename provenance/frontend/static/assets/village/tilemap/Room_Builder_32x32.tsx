f409457f1e2321e64beb0cbdfb62a1c5735ec650
Ruige <879919878@qq.com>
2026-08-30 09:14:58 +0800

    chore: 忽略 Tiled 编辑器工作文件(.tmx/.tsx/.bak 等)

--- provenance/frontend/static/assets/village/tilemap/Room_Builder_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Room_Builder" tilewidth="32" tileheight="32" tilecount="8284" columns="76">
- <image source="Room_Builder_32x32.png" width="2432" height="3488"/>
-</tileset>
